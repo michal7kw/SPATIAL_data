865f2dd8ccca7cbc8e22d5fcb74db6669e82b736
michal7kw <michal.k7w@gmail.com>
2025-05-30 14:07:58 +0200

    reun notebooks

--- py_to_ipynb.sh
+++ py_to_ipynb.sh
@@ -2,12 +2,13 @@
 
 # jupytext --to notebook p30/region_R1_analysis.py
 # jupytext --to notebook p30/region_R2_analysis.py
-jupytext --to notebook p30/region_R3_analysis.py
+# jupytext --to notebook p30/region_R3_analysis.py
 # jupytext --to notebook p30/region_R4_analysis.py
 
 # jupytext --to notebook p0/region_R1_analysis.py
 # jupytext --to notebook p0/region_R2_analysis.py
 # jupytext --to notebook p0/region_R3_analysis.py
-jupytext --to notebook p0/region_R4_analysis.py
+# jupytext --to notebook p0/region_R4_analysis.py
 
-# jupytext --to  notebook Vizgen_MERSCOPE_Vizualizer_Clustering_Template.py
+jupytext --to  notebook combined_DEG_analysis.py
+# jupytext --to  notebook combined_DEG_analysis_from_raw.py
